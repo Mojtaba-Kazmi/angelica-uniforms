0ed41f38369acc9842764638dc2659c35e166efd
mojtaba-kazmi <kazmi.saidmojtaba@gmail.com>
2024-11-29 03:08:55 +0100

    meta view added for mobile

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import CTAButton from "@/components/cta-button/CTAButton";
 import { Suspense } from "react";
 import Spinner from "@/components/spinner/Spinner";
 import { Analytics } from "@vercel/analytics/react"
+import Head from "next/head";
 import "./globals.css";
 
 export const metadata = defaultMetadata;
@@ -14,6 +15,9 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        </Head>
         <Header />
         <Suspense fallback={<Spinner />}>
           <main>
